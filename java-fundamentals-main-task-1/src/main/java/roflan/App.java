d9a7b8154e313038f73691b2fb43aaddd778700b
Igor Shemarov <hgdrtttr@gmail.com>
2020-02-20 18:40:52 +0300

    finished main task 1

---
+++ java-fundamentals-main-task-1/src/main/java/roflan/App.java
@@ -0,0 +1,12 @@
+package roflan;
+
+import java.util.Scanner;
+
+public class App
+{
+    public static void main( String[] args ) {
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+        System.out.println( "Hello " + str);
+    }
+}
